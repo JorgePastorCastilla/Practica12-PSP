b86666a99876771653c8ebc3cf39f25f8381aa34
JorgePastorCastilla <jordi131313@gmail.com>
2019-12-08 23:22:28 +0100

    no se como controlar la cola

--- src/Caixa.java
+++ src/Caixa.java
@@ -5,36 +5,44 @@ public class Caixa extends Thread{
     String nombre;
     Client cliente;
     long initialTime;
+    Semaphore semaphore;
 
-    public Caixa(String nombre,Client cliente,long initialTime) {
+    public Caixa(String nombre,Client cliente,long initialTime, Semaphore semaphore) {
         this.nombre = nombre;
         this.cliente = cliente;
         this.initialTime = initialTime;
+        this.semaphore = semaphore;
+    }
+
+    public void setCliente(Client cliente) {
+        this.cliente = cliente;
     }
 
     @Override
     public void run() {
-        Semaphore semaphore = new Semaphore();
-        if (true){
-            semaphore.acquire();
-        }else{
-            semaphore.release();
-        }
         procesarCompra(this.cliente,this.initialTime);
     }
 
     public void procesarCompra(Client client, long timeStamp) {
-        System.out.println("La cajera " + this.nombre +
-                " COMIENZA A PROCESAR LA COMPRA DEL CLIENTE " + client.getNombre() +
-                " EN EL TIEMPO: " + (System.currentTimeMillis() - timeStamp) / 1000	+ "seg");
-        for (int i = 0; i < client.getCarroCompra().length; i++) {
-            this.esperarXsegundos(client.getCarroCompra()[i]);
-            System.out.println("Procesado el producto " + (i + 1) +
-                    " ->Tiempo: " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+        try {
+
+            semaphore.acquire();
+            System.out.println("La cajera " + this.nombre +
+                    " COMIENZA A PROCESAR LA COMPRA DEL CLIENTE " + client.getNombre() +
+                    " EN EL TIEMPO: " + (System.currentTimeMillis() - timeStamp) / 1000	+ "seg");
+            for (int i = 0; i < client.getCarroCompra().length; i++) {
+                this.esperarXsegundos(client.getCarroCompra()[i]);
+//                System.out.println("Procesado el producto " + (i + 1) +
+//                        " ->Tiempo: " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+            }
+            System.out.println("La cajera " + this.nombre + " HA TERMINADO DE PROCESAR " +
+                    client.getNombre() + " EN EL TIEMPO: " +
+                    (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        System.out.println("La cajera " + this.nombre + " HA TERMINADO DE PROCESAR " +
-                client.getNombre() + " EN EL TIEMPO: " +
-                (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
+        semaphore.release();
     }
 
     private void esperarXsegundos(int segundos) {
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        /*
         Client cliente1 = new Client("Client 1", new int[] { 2, 2, 1, 5, 2, 3 });
         Client cliente2 = new Client("Client 2", new int[] { 1, 3, 5, 1, 1 });
         Client cliente3 = new Client("Client 3", new int[] { 2, 3, 3, 1, 2, 6 });
@@ -12,14 +16,70 @@ public class Main {
         Client cliente6 = new Client("Client 6", new int[] { 1, 1, 1, 1, 1 });
         Client cliente7 = new Client("Client 7", new int[] { 3, 5, 3, 5, 6, 3 });
         Client cliente8 = new Client("Client 8", new int[] { 1, 1, 1 });
+        */
+
+        Client cliente1 = new Client("Client 1", new int[] { 1, 1, 1, 1, 1, 1 });
+        Client cliente2 = new Client("Client 2", new int[] { 1, 1, 1, 1, 1 });
+        Client cliente3 = new Client("Client 3", new int[] { 1, 1, 1, 1, 1, 1 });
+        Client cliente4 = new Client("Client 4", new int[] { 1, 1, 1, 1, 1 });
+        Client cliente5 = new Client("Client 5", new int[] { 1, 1, 1, 1, 1 });
+        Client cliente6 = new Client("Client 6", new int[] { 1, 1, 1, 1, 1 });
+        Client cliente7 = new Client("Client 7", new int[] { 1, 1, 1, 1, 1, 1 });
+        Client cliente8 = new Client("Client 8", new int[] { 1, 1, 1 });
 
         long initaltime = System.currentTimeMillis();
-
-        Caixa caja1 = new Caixa("Paqui",cliente1,initaltime);
-        Caixa caja2 = new Caixa("Carmen",cliente2,initaltime);
+        //Semaphore semaphore1 = new Semaphore(1);
+        Semaphore semaphore2 = new Semaphore(2);
+        Caixa caja1 = new Caixa("Caja1",cliente1,initaltime,semaphore2);
+        Caixa caja2 = new Caixa("Caja2",cliente2,initaltime,semaphore2);
 
         caja1.start();
         caja2.start();
+
+        List<Client> clientes = new ArrayList<Client>();
+        clientes.add(cliente1);
+        clientes.add(cliente2);
+        clientes.add(cliente3);
+        clientes.add(cliente4);
+        clientes.add(cliente5);
+        clientes.add(cliente6);
+        clientes.add(cliente7);
+        clientes.add(cliente8);
+
+
+        for(int index = 2;index < 8; index++){
+            //WAITING = ESPERANDO
+            //TERMINATED = HA TERMINADO Y ESTA LIBRE
+            //TIMED_WAITING = ESTA ESPERANDO
+            //System.out.println("caja1 state : " + caja1.getState());
+            String caja1State = "caja1 state : " + caja1.getState();
+            String caja2State = "caja2 state : " + caja2.getState();
+            if(caja1.getState().toString() == "TERMINATED"){
+                caja1.setCliente(clientes.get(index));
+                caja1.run();
+            }else if (caja2.getState().toString() == "TERMINATED") {
+                caja2.setCliente(clientes.get(index));
+                caja2.start();
+            }else{
+                index--;
+            }
+        }
+        /*
+        while(true){
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("caja1 state : " + caja1.getState());
+            System.out.println("caja2 state : " + caja2.getState());
+            if(false){
+                System.out.println("caja1 interrupted");
+            }
+            if(caja2.semaphore.isFair()){
+                System.out.println("caja2 interrupted");
+            }
+        }*/
 
 
     }
